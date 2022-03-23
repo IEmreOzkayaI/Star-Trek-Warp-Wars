ae705a8449b5f3362fe87b76ee50e2fd7908ef91
IEmreOzkayaI <0emre.ozkaya0@gmail.com>
2022-03-23 22:25:33 +0300

    Update Main.java

--- STWW/src/Main.java
+++ STWW/src/Main.java
@@ -2,11 +2,10 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-          System.out.println("Hello World2");
-          System.out.println("Hello World3");
-          System.out.println("Hello World Emre");
-          System.out.println("Hello World Arda");
 
+<<<<<<< Updated upstream
           System.out.println("Hello World Baran");
+=======
+>>>>>>> Stashed changes
 	}
 }
