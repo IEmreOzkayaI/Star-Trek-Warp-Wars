c9313ee7c8c079254ded3459f89d6c4a6fdafb8c
thebarslan <barslanforbusiness@gmail.com>
2022-03-23 22:29:03 +0300

    Update Main.java
    Deneme

--- STWW/src/Main.java
+++ STWW/src/Main.java
@@ -2,10 +2,5 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-          System.out.println("Hello World2");
-          System.out.println("Hello World3");
-          System.out.println("Hello World Emre");
-
-          System.out.println("Hello World Baran Branch");
 	}
 }
