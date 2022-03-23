6e87f10c030e812b4d5c4dbb7046ce53a5a8a333
tevfikArda <tevfikardakorkmaz@gmail.com>
2022-03-23 22:34:12 +0300

    Update Main.java
    Selam verdim

--- STWW/src/Main.java
+++ STWW/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 		System.out.println("Hello World");
           System.out.println("Hello World Baran");
           System.out.println("Hello World Baran Branch");
-
+          System.out.println("Hello Arda");
 	}
 }
