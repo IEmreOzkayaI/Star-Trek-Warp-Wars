a0e881748b6e6342012830acc4f754968ce1b1f4
tevfikArda <tevfikardakorkmaz@gmail.com>
2022-03-23 22:41:18 +0300

    Update Main.java

--- STWW/src/Main.java
+++ STWW/src/Main.java
@@ -5,5 +5,6 @@ public class Main {
           System.out.println("Hello World Baran");
           System.out.println("Hello World Baran Branch");
           System.out.println("Hello Arda");
+          System.out.println("--------------");
 	}
 }
