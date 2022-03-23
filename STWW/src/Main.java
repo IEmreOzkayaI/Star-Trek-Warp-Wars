fe3dc7c584c8647ac643c6fe98279f18bf8c1c1e
IEmreOzkayaI <0emre.ozkaya0@gmail.com>
2022-03-23 23:09:51 +0300

    Update Main.java

--- STWW/src/Main.java
+++ STWW/src/Main.java
@@ -1,11 +1,24 @@
+import tools.ConsolePrinter;
+import tools.FileReader;
 
 public class Main {
+<<<<<<< Updated upstream
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 
 <<<<<<< Updated upstream
           System.out.println("Hello World Baran");
 =======
+>>>>>>> Stashed changes
+=======
+	public static void main(String[] args) throws Exception {
+		ConsolePrinter cp = new ConsolePrinter();
+		FileReader file = new FileReader();
+		String [] map = file.readFile("maze.txt");
+		for (int i = 0; i < map.length; i++) {
+			System.out.println(map[i]);
+		}
+		
 >>>>>>> Stashed changes
 	}
 }
